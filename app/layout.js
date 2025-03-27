846622db9079ba7d6d0c110961d76f86a2cf5a73
Ian Tan <iantxh@gmail.com>
2025-03-27 22:13:00 +0800

    Fixed picture lines

--- app/layout.js
+++ app/layout.js
@@ -5,9 +5,12 @@ import Cats from 'components/cats/cats';
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=0"/>
+      </head>
       <body >
+      <Cats />
         <div className="background">
-          <Cats />
           {children}
         </div>
       </body>
--- components/home/hero.jsx
+++ components/home/hero.jsx
@@ -19,32 +19,35 @@ export default function Hero() {
 
     return (
         <div className='test-container'>
-            <hr className='picture-line style1' />
-            <div className='picture-wrapper picline1-pic1'>
+            <div className='picture-line-wrapper'>
+                <hr className='picture-line style1' />
+            </div>
+            <div className='picture-wrapper picline1-pic1 picline1'>
                 <img className='picture' src='./images/us/us1.jpg' alt='us1' />
             </div>
-            <div className='picture-wrapper picline1-pic2' >
+            <div className='picture-wrapper picline1-pic2 picline1' >
                 <img className='picture' src='./images/us/us2.jpg' alt='us2' />
             </div>
-            <div className='picture-wrapper picline1-pic3'>
+            <div className='picture-wrapper picline1-pic3 picline1'>
                 <img className='picture' src='./images/us/us3.jpg' alt='us3' />
             </div>
-            <div className='picture-wrapper picline1-pic4'>
+            <div className='picture-wrapper picline1-pic4 picline1'>
                 <img className='picture' src='./images/us/us4.jpg' alt='us4' />
             </div>
-            <hr className='picture-line style2' />
 
-
-            <div className='picture-wrapper picline2-pic1'>
+            <div className='picture-line-wrapper'>
+                <hr className='picture-line style2' />
+            </div>
+            <div className='picture-wrapper picline2-pic1 picline2'>
                 <img className='picture' src='./images/us/us5.jpg' alt='us5' />
             </div>
-            <div className='picture-wrapper picline2-pic2' >
+            <div className='picture-wrapper picline2-pic2 picline2' >
                 <img className='picture' src='./images/us/us6.jpg' alt='us6' />
             </div>
-            <div className='picture-wrapper picline2-pic3'>
+            <div className='picture-wrapper picline2-pic3 picline2'>
                 <img className='picture' src='./images/us/us7.jpg' alt='us7' />
             </div>
-            <div className='picture-wrapper picline2-pic4'>
+            <div className='picture-wrapper picline2-pic4 picline2'>
                 <img className='picture' src='./images/us/us8.jpg' alt='us8' />
             </div>
 
